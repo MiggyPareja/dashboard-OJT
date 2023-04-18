bdf0c839f69925e575aeaf871256b90b6d40f876
Your Name <you@example.com>
2023-04-18 10:34:13 +0800

    updated

--- app/Views/index.php
+++ app/Views/index.php
@@ -24,7 +24,7 @@
 <?php endif; ?>  
 <?php if (session()->getFlashdata('query')): ?>
     <div class="alert alert-success timer"><?= session()->getFlashdata('query') ?></div>
-<?php endif; ?>    
+<?php endif; ?> 
 <!-- ADD MODAL -->
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
